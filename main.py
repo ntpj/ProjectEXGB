c5db2230b298c1ccc8a26dc5f725066e87e91911
NuntapongJ <98025077+NuntapongJ@users.noreply.github.com>
2024-03-03 21:52:17 +0700

    Class Implentations
    Add a bit more function to get the user's info (temporary)

--- main.py
+++ main.py
@@ -5,53 +5,78 @@ import sys
 import discord
 from discord.ext import commands
 from discord import FFmpegPCMAudio
+import asyncio
 import youtube_dl
 
 sys.path.append('./functions')
 from functions.mp3_player import audio_check
 from functions.logMsg import log, logtb
+from exgbClass import user
 
+
+##### Start 
 TOKEN = 'TOKEN'
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-# Online check
+##### Event
 @bot.event
 async def on_ready():
     log(f'Logged in as {bot.user}')
     
-# %% Functions
-if True: # Not working
-    @bot.command(name='play')
-    async def play(ctx, url):
-        try:
-            voice_state = ctx.author.voice
-            if voice_state is None:
-                await ctx.send("You need to be in a voice channel to use this command.")
-            else:
-                voice_channel = ctx.author.voice.channel
-                if voice_channel:
-                    music = audio_check(url)
-                    vc = await voice_channel.connect()
-                    vc.play(discord.FFmpegPCMAudio(music))
-                    os.remove(music)
-        except:
-            await ctx.send("Error")
-            await ctx.send(f"```{traceback.format_exc()}```")
-            logtb(traceback.format_exc())
+    
+# %% Function
 
-    @bot.command() # Not working
-    async def leave(ctx):
-        voice_client = ctx.guild.voice_client
-        if voice_client:
-            await voice_client.disconnect()
+@bot.event
+async def on_message(message):
+    user.data = message
+    user.id = message.author.id
+    user.name = message.author.name
+    user.avatar = message.author.avatar
+    user.global_name = message.author.global_name
+    if message.content.startswith('!'):
+        ctx = await bot.get_context(message)
+        await bot.invoke(ctx)
+   
+@bot.command()     
+async def play(ctx, url):
+    try:
+        voice_state = ctx.author.voice
+        if voice_state is None:
+            await ctx.send("You need to be in a voice channel to use this command.")
+        else:
+            voice_channel = ctx.author.voice.channel
+            if voice_channel:
+                music = audio_check(url)
+                vc = await voice_channel.connect()
+                vc.play(discord.FFmpegPCMAudio(music))
+                os.remove(music)
+    except:
+        await ctx.send("Error")
+        await ctx.send(f"```{traceback.format_exc()}```")
+        logtb(traceback.format_exc())
 
-    # Test command
-    @bot.command(name="ping")
-    async def ping(ctx):
-        log("Reply to ping")
-        await ctx.send("OK")
+@bot.command() # Not working
+async def leave(ctx):
+    voice_client = ctx.guild.voice_client
+    if voice_client:
+        await voice_client.disconnect()
+        
+@bot.command()
+async def pinfo(ctx, type: str='short'):
+    if type == "short":
+        await ctx.send(f"```UID : {user.id}\nName: {user.name}\nAlias: {user.global_name}```{user.avatar}")
+    elif type == "msg_data":
+        await ctx.send(f"```Message data:\n{user.data}```")
+        
 
-# %% Run
+# %% Main
+
+# Test command
+@bot.command(name="ping")
+async def ping(ctx):
+    log("Reply to ping")
+    await ctx.send("OK")
 
 if __name__ == "__main__":
+    user = user()
     bot.run(TOKEN)
